01db1abbc40ad604892473bf115ba59ec17cde06
Jedrzej Kosinski <kosinkadink1@gmail.com>
2023-08-29 05:07:55 -0500

    Updated nodes.py to account for recent comfy changes

--- animatediff/nodes.py
+++ animatediff/nodes.py
@@ -14,7 +14,8 @@ import comfy.model_management as model_management
 from comfy.ldm.modules.attention import SpatialTransformer
 from comfy.ldm.modules.diffusionmodules.util import GroupNorm32
 from comfy.utils import load_torch_file, calculate_parameters
-from comfy.sd import VAE, ModelPatcher, load_checkpoint_guess_config
+from comfy.sd import VAE, load_checkpoint_guess_config
+from comfy.model_patcher import ModelPatcher
 
 from .logger import logger
 from .motion_module import MotionWrapper, VanillaTemporalModule
