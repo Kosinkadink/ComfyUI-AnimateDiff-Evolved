9f84dd29fb8f2ddd57364d3c8499ea2f18964605
Jedrzej Kosinski <kosinkadink1@gmail.com>
2023-10-11 20:36:13 -0500

    Updated motion_utils.py code to work with recent comfy update

--- animatediff/motion_utils.py
+++ animatediff/motion_utils.py
@@ -2,26 +2,56 @@ import torch
 from torch import Tensor, nn
 from abc import ABC, abstractmethod
 
-from comfy.ldm.modules.attention import CrossAttentionBirchSan, CrossAttentionDoggettx, CrossAttentionPytorch, FeedForward, CrossAttention, MemoryEfficientCrossAttention
+from comfy.ldm.modules.attention import attention_basic, attention_pytorch, attention_split, attention_sub_quad, attention_xformers, default
 import comfy.model_management as model_management
+import comfy.ops
 from comfy.cli_args import args
 
 from .motion_lora import MotionLoRAInfo
 
 
-CrossAttentionMM = CrossAttention
 # until xformers bug is fixed, do not use xformers for VersatileAttention! TODO: change this when fix is out
-# logic for choosing CrossAttention method taken from comfy/ldm/modules/attention.py
+# logic for choosing optimized_attention method taken from comfy/ldm/modules/attention.py
+optimized_attention_mm = attention_basic
 if model_management.xformers_enabled():
     pass
-    # CrossAttentionMM = MemoryEfficientCrossAttention
+    #optimized_attention_mm = attention_xformers
 if model_management.pytorch_attention_enabled():
-    CrossAttentionMM = CrossAttentionPytorch
+    optimized_attention_mm = attention_pytorch
 else:
     if args.use_split_cross_attention:
-        CrossAttentionMM = CrossAttentionDoggettx
+        optimized_attention_mm = attention_split
     else:
-        CrossAttentionMM = CrossAttentionBirchSan
+        optimized_attention_mm = attention_sub_quad
+
+
+class CrossAttentionMM(nn.Module):
+    def __init__(self, query_dim, context_dim=None, heads=8, dim_head=64, dropout=0., dtype=None, device=None, operations=comfy.ops):
+        super().__init__()
+        inner_dim = dim_head * heads
+        context_dim = default(context_dim, query_dim)
+
+        self.heads = heads
+        self.dim_head = dim_head
+
+        self.to_q = operations.Linear(query_dim, inner_dim, bias=False, dtype=dtype, device=device)
+        self.to_k = operations.Linear(context_dim, inner_dim, bias=False, dtype=dtype, device=device)
+        self.to_v = operations.Linear(context_dim, inner_dim, bias=False, dtype=dtype, device=device)
+
+        self.to_out = nn.Sequential(operations.Linear(inner_dim, query_dim, dtype=dtype, device=device), nn.Dropout(dropout))
+
+    def forward(self, x, context=None, value=None, mask=None):
+        q = self.to_q(x)
+        context = default(context, x)
+        k = self.to_k(context)
+        if value is not None:
+            v = self.to_v(value)
+            del value
+        else:
+            v = self.to_v(context)
+
+        out = optimized_attention_mm(q, k, v, self.heads, mask)
+        return self.to_out(out)
 
 
 class BlockType:
