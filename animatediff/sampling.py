73d96abadf72ff3c54e649f7a44cedfa8ae17398
Jedrzej Kosinski <kosinkadink1@gmail.com>
2023-11-26 03:48:02 -0600

    Account for ComfyUI current_index rename to transformer_index

--- animatediff/sampling.py
+++ animatediff/sampling.py
@@ -104,10 +104,15 @@ def forward_timestep_embed_factory() -> Callable:
                     x = layer(x, context)
                 elif isinstance(layer, attention.SpatialVideoTransformer):
                     x = layer(x, context, time_context, num_video_frames, image_only_indicator, transformer_options)
-                    transformer_options["current_index"] += 1
+                    if "transformer_index" in transformer_options:
+                        transformer_options["transformer_index"] += 1
+                    if "current_index" in transformer_options: # keep this for backward compat, for now
+                        transformer_options["current_index"] += 1
                 elif isinstance(layer, attention.SpatialTransformer):
                     x = layer(x, context, transformer_options)
-                    if "current_index" in transformer_options:
+                    if "transformer_index" in transformer_options:
+                        transformer_options["transformer_index"] += 1
+                    if "current_index" in transformer_options:  # keep this for backward compat, for now
                         transformer_options["current_index"] += 1
                 elif isinstance(layer, openaimodel.Upsample):
                     x = layer(x, output_shape=output_shape)
