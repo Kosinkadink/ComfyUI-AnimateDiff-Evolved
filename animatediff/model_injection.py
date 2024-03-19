e7a61d8ef2a868e0865b7a9079be0db521e6127d
Jedrzej Kosinski <kosinkadink1@gmail.com>
2024-03-19 12:53:46 -0500

    Add patch_weights to ModelPatcherAndInjector to work with lowvram

--- animatediff/model_injection.py
+++ animatediff/model_injection.py
@@ -49,9 +49,9 @@ class ModelPatcherAndInjector(ModelPatcher):
                 except Exception:
                     pass
 
-    def patch_model(self, device_to=None):
+    def patch_model(self, device_to=None, patch_weights=True):
         # first, perform model patching
-        patched_model = super().patch_model(device_to)
+        patched_model = super().patch_model(device_to, patch_weights)
         # finally, perform motion model injection
         self.inject_model(device_to=device_to)
         return patched_model
